54aa1b8c6161d0ca114824aee42dc455b20e3073
B4Hive <b4.bruno.santos@gmail.com>
2024-12-06 21:17:28 -0300

    edicao em main.cpp, CMakeLists.txt, .gitignore
    adicionando a pasta build no git ignore, colocando mensagem de erro caso não sejam fornecidos argumentos para main e corrigido CMakeLists

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-
+    if(argc < 3 || argc > 4){
+        std::cout << "Input error: missing arguments" << std::endl;
+        return 1;
+    }
+    
     std::string filename;
     filename = argv[3];
     Grafo *g = new Grafo(filename);
